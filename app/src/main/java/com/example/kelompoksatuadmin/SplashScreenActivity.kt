4a438991e5025d644d31c913e20aac151ce3b1bf
ricorizkya <ricorizkya26@gmail.com>
2021-09-24 22:32:24 +0700

    Add SplashScreenActivity.kt

--- app/src/main/java/com/example/kelompoksatuadmin/SplashScreenActivity.kt
+++ app/src/main/java/com/example/kelompoksatuadmin/SplashScreenActivity.kt
@@ -1,11 +1,41 @@
 package com.example.kelompoksatuadmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+import com.example.kelompoksatuadmin.databinding.ActivitySplashScreenBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        actionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        binding.splashSreen.alpha = 0f
+        binding.splashSreen.animate().setDuration(1500).alpha(1f).withEndAction {
+            Handler().postDelayed({
+                                  if (user != null) {
+                                      startActivity(Intent(this, MainActivity::class.java))
+                                      overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                                      finish()
+                                  }else {
+                                      startActivity(Intent(this, MainActivity::class.java))
+                                      overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                                      finish()
+                                  }
+            },200)
+        }
     }
 }
